25aef2b5b0232edeb9ddea540b79554493741da2
chen <lu.chen@liketry.com>
2016-10-24 15:12:33 +0800

    main

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'CH-LU',
+        'brandLabel' => 'Ada 绣',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
