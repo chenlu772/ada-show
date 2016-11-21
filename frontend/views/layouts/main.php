0e07ad7f3e537dc72d69d60eb21bc473abf06464
chen <lu.chen@liketry.com>
2016-11-21 15:30:44 +0800

    main

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -11,7 +11,7 @@ use frontend\assets\AppAsset;
 use common\widgets\Alert;
 
 AppAsset::register($this);
-$this->title = 'Ada 绣';
+$this->title = 'CH-LU';
 ?>
 <?php $this->beginPage() ?>
 <!DOCTYPE html>
@@ -29,25 +29,25 @@ $this->title = 'Ada 绣';
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'Ada 绣',
+        'brandLabel' => Html::img('favicon.ico'),
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
     $menuItems = [
-        ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
-        ['label' => 'Contact', 'url' => ['/site/contact']],
+        ['label' => '首页', 'url' => ['/site/index']],
+        ['label' => '关于我们', 'url' => ['/site/about']],
+        ['label' => '联系我们', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
-        $menuItems[] = ['label' => 'Signup', 'url' => ['/site/signup']];
-        $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
+        $menuItems[] = ['label' => '注册', 'url' => ['/site/signup']];
+        $menuItems[] = ['label' => '登录', 'url' => ['/site/login']];
     } else {
         $menuItems[] = '<li>'
             . Html::beginForm(['/site/logout'], 'post')
             . Html::submitButton(
-                'Logout (' . Yii::$app->user->identity->username . ')',
+                '退出 (' . Yii::$app->user->identity->username . ')',
                 ['class' => 'btn btn-link']
             )
             . Html::endForm()
