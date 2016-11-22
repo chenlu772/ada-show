ef94bbc82f787c3f6fb84f8c1030176f99e5dcc0
chen <lu.chen@liketry.com>
2016-11-22 16:07:04 +0800

    contact

--- frontend/models/ContactForm.php
+++ frontend/models/ContactForm.php
@@ -52,7 +52,7 @@ class ContactForm extends Model
     {
         return Yii::$app->mailer->compose()
             ->setTo($email)
-            ->setFrom([$this->email => $this->name])
+            //->setFrom([$this->email => $this->name])
             ->setSubject($this->subject)
             ->setTextBody($this->body)
             ->send();
