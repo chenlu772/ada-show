771bdcae1b4b2f4ca7b8ea42fe16075f51cc1ffe
chen <lu.chen@liketry.com>
2016-12-29 15:00:22 +0800

    color

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ $(function(){
 		$(this).animate({fontSize:'18px'},100)
 	});
 	$('#main p a').mouseout(function(){
-		$(this).css('color','#555');
+		$(this).css('color','#000');
 		$(this).animate({fontSize:'14px'},10)
 	});
 });
