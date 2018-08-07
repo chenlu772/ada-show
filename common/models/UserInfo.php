346a5980a01edbaee90c498ffd130c6b3288aad9
chenlu772 <chenlu772@foxmail.com>
2018-08-07 16:38:58 +0800

    mm

--- common/models/UserInfo.php
+++ common/models/UserInfo.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -43,50 +42,4 @@ class UserInfo extends \yii\db\ActiveRecord
         ];
     }
 
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: chen
- * Date: 2018/7/28
- * Time: 18:12
- */
-
-namespace common\models;
-
-class UserInfo extends \yii\db\ActiveRecord
-{
-
-
-    public static function tableName()
-    {
-        return 'user_info';
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function rules()
-    {
-        return [
-            [['user_id'], 'integer'],
-            [['open_id', 'union_id', 'session_key'], 'string', 'max' => 64],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function attributeLabels()
-    {
-        return [
-            'user_id' => 'ID',
-            'open_id' => 'openId',
-            'union_id' => 'unionId',
-            'session_key' => '',
-            'create_at' => '创建时间',
-        ];
-    }
-
->>>>>>> 7f73e4eca1eb12e73efc533695b3f68c42c055d5
 }
--- frontend/controllers/PoetryController.php
+++ frontend/controllers/PoetryController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 /**
  * Created by PhpStorm.
@@ -41,58 +40,4 @@ class PoetryController extends BaseController{
 
         return json_encode($poetry);
     }
-
-
-
-
-
-=======
-<?php
-/**
- * Created by PhpStorm.
- * User: chen
- * Date: 2018/7/27
- * Time: 11:47
- */
-namespace frontend\controllers;
-
-use frontend\models\Poetry;
-use Yii;
-use common\models\weChat;
-
-class PoetryController extends BaseController{
-
-
-    public function actionIndex()
-    {
-        return $this->render('index');
-    }
-
-    public function actionOnLogin(){
-
-        $code = Yii::$app->request->get('code');
-
-        $model = new weChat();
-        $wechatData = $model->jscode2session($code);
-
-        if ($wechatData){
-            return json_encode($wechatData);
-        }
-
-        return false;
-    }
-
-    public function actionOnePoetry(){
-
-        $model = new Poetry();
-        $poetry= $model->getOnePoetry();
-
-        return json_encode($poetry);
-    }
-
-
-
-
-
->>>>>>> 7f73e4eca1eb12e73efc533695b3f68c42c055d5
 }
